d02b07017e339743df1f68380d8556da6e9cf469
Jennifer Chen <shinyi.chen@gmail.com>
2019-10-21 16:30:11 -0700

    feat: added service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: "./src/client/index.js",
@@ -35,6 +36,7 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        })
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
