eb9c0814e986fc08cde91c04ecd7e7d728123ad7
Jennifer Chen <shinyi.chen@gmail.com>
2019-10-22 11:49:17 -0700

    feat: get date and countdown

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,42 @@
 import { getCoord } from "./js/getCoordinate";
 import "./styles/styles.scss";
-export {
-    getCoord
-}
+
+document.getElementById("submitButton").addEventListener("click", function(event) {
+    event.preventDefault();
+
+    // today's date
+    let today = new Date(Date.now());
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+
+    // get user inputs
+    const city = document.getElementById("cityInput").value;
+    const date = document.getElementById("dateInput").value;
+
+    // get travel date
+    const dateSplit = date.split("-");
+    let travelDate = new Date(dateSplit[0], dateSplit[1]-1, dateSplit[2], 0, 0, 0, 0);
+
+    // count the difference in days
+    const daysAway = (travelDate.getTime() - today.getTime())/(1000*60*60*24);
+
+    // get coordinate from city
+    getCoord(city).then((coordRecord) => {
+        if (coordRecord.status === "ok") {
+            console.log(coordRecord);
+            document.getElementById("countdown").innerHTML = daysAway;
+            document.getElementById("city").innerHTML = coordRecord.name;
+            document.getElementById("country").innerHTML = coordRecord.countryName;
+            document.getElementById("lat").innerHTML = coordRecord.lat;
+            document.getElementById("lon").innerHTML = coordRecord.lng;
+        } else {
+            console.log("invalid");
+        }
+    });
+});
+
+// export {
+//     getCoord
+// }
--- src/client/js/getCoordinate.js
+++ src/client/js/getCoordinate.js
@@ -1,25 +1,17 @@
-function getCoord(event) {
-    event.preventDefault();
+async function getCoord(city) {
 
     const GEONAMES_ENDPOINT = "http://api.geonames.org/searchJSON?";
     const GEONAMES_USER = "shinyichen";
-    const city = document.getElementById("cityInput").value;
     const url = encodeURI(`${GEONAMES_ENDPOINT}q=${city}&maxRows=1&style=MEDIUM&lang=en&username=${GEONAMES_USER}`);
-    fetch(url)
-    .then((response) => {
-        if (response.status === 200){ 
-            response.json().then(data => {
-                const record = data.geonames[0];
-                document.getElementById("city").innerHTML = record.name;
-                document.getElementById("country").innerHTML = record.countryName;
-                document.getElementById("lat").innerHTML = record.lat;
-                document.getElementById("lon").innerHTML = record.lng;
-            })
-        } else {
-            console.log("error");
-        }
-        
-    });
+    const response = await fetch(url)
+    if (response.status === 200) { 
+        const data = await response.json();
+        const record = data.geonames[0];
+        record.status = "ok";
+        return record;
+    } else {
+        return { status: "error" };
+    }
 }
 
 export {
